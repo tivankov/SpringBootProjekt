30d733b5036c3b9dbde7e0fd505ca8c1bc9ec776
tivankov <tivankov21@student.unizd.hr>
2023-03-17 13:24:56 +0100

    Dodana stranica pizza error,
    Error stranica se namjerno nalazi u static folderu a ne u templates zato da se vidi error code priliko debuga stranice.
    Edit i create promjenjeni kako bi se vidjele stranice (ne rade)

--- PizzaPerfetta/src/main/java/hr/unizd/springprojekt/PizzaPerfetta/create/PizzaCreateForm.java
+++ PizzaPerfetta/src/main/java/hr/unizd/springprojekt/PizzaPerfetta/create/PizzaCreateForm.java
@@ -3,8 +3,9 @@ package hr.unizd.springprojekt.PizzaPerfetta.create;
 import jakarta.validation.constraints.NotEmpty;
 
 public class PizzaCreateForm {
-
-    @NotEmpty(message = "Pizza must haave name")
+    @NotEmpty(message = "Pizza must have picture")
+    private String PizzaPhotoLink;
+    @NotEmpty(message = "Pizza must have name")
     private String name;
 
     @NotEmpty(message = "Pizza must have at least 1 ingredient")
@@ -12,6 +13,14 @@ public class PizzaCreateForm {
 
     public String getName() {
         return name;
+    }
+
+    public String getPizzaPhotoLink() {
+        return PizzaPhotoLink;
+    }
+
+    public void setPizzaPhotoLink(String pizzaPhotoLink) {
+        PizzaPhotoLink = pizzaPhotoLink;
     }
 
     public void setName(String name) {
